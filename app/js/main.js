4fdec86213ae4e7fcca761a1b302703db74c1786
Lenseg <Lenseg1@gmail.com>
2016-04-28 07:41:12 +0300

    chronics module

--- app/js/main.js
+++ app/js/main.js
@@ -28,9 +28,11 @@ ioann.controller('familyCtrl',function ($scope) {
 });
 ioann.controller('celebrationsCtrl',['$scope', '$http', function ($scope, $http) {
 	$scope.showpers = false;
+	$scope.showchrons = false;
 	$scope.personalys=[];
+	$scope.chronics=[];
 	$scope.showpeople = function(){
-		if(!$scope.personalys.lenght)
+		if($scope.personalys.length === 0)
 			$http.get('/data/personalys.json').then(
 				function(res){
 					$scope.personalys = res.data;
@@ -45,5 +47,24 @@ ioann.controller('celebrationsCtrl',['$scope', '$http', function ($scope, $http)
 			value.visible = false;
 		});
 		mainpersonaly.visible = true;
+	}
+	$scope.showhroniks = function(){
+		if($scope.chronics.length === 0)
+			$http.get('/data/chronics.json').then(
+				function(res){
+					$scope.chronics = res.data;
+					$scope.chronics[Object.keys($scope.chronics)[0]].visible=true;
+					$scope.chronicsvideo = $scope.chronics[Object.keys($scope.chronics)[0]].video;
+					$scope.showchrons = !$scope.showchrons;
+				}
+			)
+		else
+			$scope.showchrons = !$scope.showchrons;
+	}
+	$scope.selectChronic = function (chronic) {
+		angular.forEach($scope.chronics,function(value){
+			value.visible = false;
+		});
+		chronic.visible = true;
 	}
 }]);
